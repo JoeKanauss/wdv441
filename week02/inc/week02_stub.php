0e6f8a2b7e813a2b77384596958444f6bfe2b8d3
JoeKanauss <33582866+JoeKanauss@users.noreply.github.com>
2018-02-11 10:51:28 -0600

    2/11/18 commit

---
+++ week02/inc/week02_stub.php
@@ -0,0 +1,25 @@
+<?php
+/*
+	user_id
+	first_name
+	last_name
+	email
+	phone
+	password
+*/	
+
+function load(user_id)
+{
+	// use the user_id to search for and load any records that it matches
+}
+
+function save(user_id, first_name, last_name, email, phone, password)
+{
+	// using the record that matches the user_id, update the long-term storage with the first_name, last_name, email, phone, and password values
+}
+
+function validate(first_name, last_name, email, phone, password)
+{
+	// using validation functions, validate the first_name, last_name, email, phone, and password values
+}
+?>
